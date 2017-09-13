f954a34ef883f09bb5158ddc21620573dcd02a99
JaceGarcia <jaces.place@gmail.com>
2017-09-13 18:54:43 -0400

    created a user through my forms

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -28,17 +28,26 @@ class UserProfile extends Component {
             await this.setState({error: err.message})
             return err.message
         }
+        
     }
+    
 
     render () {
+        const imageStyles = {
+            width: "300",
+            height: "300"
+        }
         const id = this.props.match.params.id            
         return (
             <div>
+            <img style={imageStyles} src={this.state.user.image} alt=''/>
             <h1>{this.state.user.name}</h1>
+            <h3>Age: {this.state.user.age}</h3>
             <h3>Height: {this.state.user.height}ft</h3>
             <h3>Gender: {this.state.user.gender}</h3>
             <h3>Body Type: {this.state.user.body_type}</h3>
             <h3>Linked In: <a href={this.state.user.linked_in}>{this.state.user.linked_in}</a></h3>
+            <h3>Bio: {this.state.user.bio}</h3>
 
                 
             </div>
--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -12,25 +12,34 @@ class SignUpLogIn extends Component {
        redirect: false
    }
  }
-
- _signUp = (e) => {
-   e.preventDefault();
-   this.setState({redirect: true})
- }
-
- _signIn = (e) => {
-   e.preventDefault();
-   this.setState({redirect: true})
- }
-
  _handleChange = (e) => {
    const newState = {...this.state};
    newState[e.target.name] = e.target.value;
    this.setState(newState);
  }
+ _handleSubmit = (e) => {
+  e.preventDefault();
+  axios.post("/auth", this.state).then((res) => {
+
+      console.log("Success!");
+  })
+      .catch(err => console.log(err));
+};
 
 
  render() {
+  const buttonStyles = {
+    borderRadius: "5px",
+    fontSize: "15px",
+    textDecoration: "none",
+    margin: "20px",
+    color: "#000",
+    position: "relative",
+    display: "flex",
+    backgroundColor: "#fff",
+    alignContent: "center"
+}
+
    if (this.state.redirect){
      return <Redirect to="/" />
    }
@@ -39,23 +48,47 @@ class SignUpLogIn extends Component {
        <form onSubmit={this._signUp}>
          <div>
            <label htmlFor="email">E-mail: </label>
-           <input onChange={this._handleChange} type="text" name="email" value={this.state.email} />
-         </div>
-         <div>
-           <label htmlFor="email">Confirm Email: </label>
-           <input onChange={this._handleChange} type="text" name="email_confirmation" value={this.state.email_confirmation} />
+           <input style={buttonStyles} onChange={this._handleChange} type="text" name="email" value={this.state.email} />
          </div>
          <div>
            <label htmlFor="password">Password: </label>
-           <input onChange={this._handleChange} type="text" name="password" value={this.state.password} />
+           <input style={buttonStyles} onChange={this._handleChange} type="text" name="password" value={this.state.password} />
          </div>
          <div>
            <label htmlFor="password">Confirm Password: </label>
-           <input onChange={this._handleChange} type="text" name="password_confirmation" value={this.state.password_confirmation} />
+           <input style={buttonStyles} onChange={this._handleChange} type="text" name="password_confirmation" value={this.state.password_confirmation} />
+         </div>
+         <div>
+           <label htmlFor="image">Profile Picture: </label>
+           <input style={buttonStyles} onChange={this._handleChange} type="text" name="image" value={this.state.image} />
+         </div>
+         <div>
+           <label htmlFor="gender">Gender: </label>
+           <input style={buttonStyles} onChange={this._handleChange} type="text" name="gender" value={this.state.gender} />
+         </div>
+         <div>
+           <label htmlFor="height">Height: </label>
+           <input style={buttonStyles} onChange={this._handleChange} type="text" name="height" value={this.state.height} />
+         </div>
+         <div>
+           <label htmlFor="age">Age: </label>
+           <input style={buttonStyles} onChange={this._handleChange} type="text" name="age" value={this.state.age} />
+         </div>
+         <div>
+           <label htmlFor="body_type">Body Type: </label>
+           <input style={buttonStyles} onChange={this._handleChange} type="text" name="body_type" value={this.state.body_type} />
+         </div>
+         <div>
+           <label htmlFor="linked_in">Linked In: </label>
+           <input style={buttonStyles} onChange={this._handleChange} type="text" name="linked_in" value={this.state.linked_in} />
+         </div>
+         <div>
+           <label htmlFor="bio">Bio: </label>
+           <textarea style={buttonStyles} onChange={this._handleChange} type="text" name="bio" maxlength="255" value={this.state.bio} />
          </div>
          
-         <button>Sign Up</button>
-         <button onClick={this._signIn}>Log In</button>
+      <input style={buttonStyles} onClick={this._handleSubmit} type="submit" />
+
        </form>
      </div>
    );
